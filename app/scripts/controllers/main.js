f9f6488395b10f88c83f3b5cd65125b53f8e2215
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-02-09 12:55:05 +0900

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('lgtmApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
