7d04b1ed9c915429e4d7d8d90a79354e9ae0b556
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-02-15 22:08:41 +0900

    add test

--- test/spec/controllers/show.js
+++ test/spec/controllers/show.js
@@ -1,28 +1,67 @@
 'use strict';
 
-describe('Controller: ShowCtrl', function () {
+describe('Controller: ShowCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('lgtmApp'));
+    // load the controller's module
+    beforeEach(module('lgtmApp'));
 
-  var ShowCtrl,
-    scope,
-    $httpBackend;
+    var ShowCtrl,
+        scope, routeParams,
+        $httpBackend, $controller, $rootScope, $routeParams, $location;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/awesomeThings')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
-    scope = $rootScope.$new();
-    ShowCtrl = $controller('ShowCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function(_$httpBackend_, _$controller_, _$rootScope_, _$routeParams_, _$location_) {
+        $httpBackend = _$httpBackend_;
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        $routeParams = _$routeParams_;
+        $location = _$location_;
+        scope = $rootScope.$new();
+        routeParams = {
+            id: 1
+        };
+        ShowCtrl = $controller('ShowCtrl', {
+            $routeParams: routeParams,
+            $scope: scope
+        });
+    }));
+
+    afterEach(function() {
+
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings).toBeUndefined();
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
+    it('should be able to obtain the required parameters', function() {
+        $httpBackend.expectGET('/api/image/1')
+            .respond(200, {
+                src: 'test'
+            });
+        expect(scope.image).toBeUndefined();
+        expect(scope.content).toBeUndefined();
+        $httpBackend.flush();
+        expect(scope.image).toEqual({
+            src: 'test'
+        });
+        expect(scope.content).toEqual('![LGTM](test)');
+
+    });
+    it('should be redirected to the top page have set the invalid id', function() {
+        $httpBackend.expectGET('/api/image/1')
+            .respond(400, null);
+        $httpBackend.flush();
+        expect($location.path()).toBe('/');
+    });
+
+    it('should have a post request, be redirected to the top page', function() {
+        /* テストの仕方がよくわからん....
+        $httpBackend.expectDELETE('/api/image/1')
+            .respond(200);
+        expect(scope.image).toBeUndefined();
+        expect(scope.content).toBeUndefined();
+        $httpBackend.flush();
+        console.log(scope.onDelete());
+        expect($location.path()).toBe('/');
+        */
+    });
+
+
 });
