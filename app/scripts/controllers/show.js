ca31075cb3e99138c78c78e4fd1c2a1f85bf8981
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-02-15 15:57:32 +0900

    Add an image deletion function

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -5,6 +5,16 @@ angular.module('lgtmApp')
         var id = $routeParams.id;
         $http.get('/api/image/' + id).success(function(image) {
             $scope.image = image;
-            $scope.content = '![LGTM](' + image.url + ')';
+            $scope.content = '![LGTM](' + image.src + ')';
         });
+
+        $scope.onDelete = function() {
+            console.log(id);
+            $http.delete('/api/image/' + id).success(function(image) {
+                console.log('success');
+
+            });
+
+
+        };
     });
